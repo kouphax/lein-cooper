10b746d52148ade9f2a02ebce8a74ff78827c956
James Hughes <james@yobriefca.se>
2014-05-30 09:02:07 +0100

    Code tidy up and UX work
    lein-cooper now checks for the existence of the Procfile and fails
    gracefully if it does not exist. There could be more work done here to
    ensure that the Procfile is correctly formatted etc but that is for
    another time.
    
    A disclaimer has been added when a process exists outside the control of
    cooper.  The problem is any child processes spawned (e.g. lein simpleton
    will create a second process that does the web server work) aren't
    killed when .destroy() is called on their parents and the JVM provides
    no way to get access to subprocesses leaving orphaned processes and
    giveing everyone a hard time.
    
    Generally tidied up the overall flow and structure thanks to doto

--- src/leiningen/cooper.clj
+++ src/leiningen/cooper.clj
@@ -1,23 +1,24 @@
 (ns leiningen.cooper
   (:require [me.raynes.conch.low-level :as sh]
             [primrose.core :as primrose]
-            [clojure.java.io :refer [reader]]
+            [clojure.java.io :as io]
             [clansi :refer [style]]
-            [clojure.string :refer [split trim join]]))
+            [clojure.string :refer [split trim join]]
+            [leiningen.core.main :refer [abort]]))
 
 (def pallette (cycle [:green :blue :yellow :magenta :red]))
 
 (defn pretty-pipe [process-name process colour]
   (doseq [stream [(:out process) (:err process)]]
     (future
-      (let [output (reader stream) ]
+      (let [output (io/reader stream) ]
         (loop [out (.readLine output)]
           (when-not (nil? out)
             (println (str (style process-name colour) ": " out)))
           (recur (.readLine output)))))))
 
 (defn get-procs []
-  (with-open [buffer (reader "Procfile")]
+  (with-open [buffer (io/reader "Procfile")]
     (doall
       (for [line (line-seq buffer)]
         (let [matcher (re-matcher #"([^:]*):(.*)" line)
@@ -31,16 +32,32 @@
     (doall
       (map pretty-pipe names procs colours))))
 
-(defn cooper
-  "I don't do a lot."
-  [project & args]
-  (let [procs (get-procs)]
-    (pipe-procs procs)
-    (deref
-      (apply
-        primrose/first
-        (map #(future (sh/exit-code %)) procs)))
+(defn ensure-procfile-exists []
+  (when-not (.exists (io/file "Procfile"))
+    (println (style "Procfile does not exist" :red))
+    (abort)))
+
+(defn fail [procs]
+  (println (style "
+One or more processes have stopped running. In the current
+version of lein-cooper this could result in child processes
+(i.e. ones spawned by a process that lein-cooper manages)
+left hanging that may need killed manually
+
+Sorry about the inconvenience." :red))
+    (println)
     (doseq [proc procs]
-      (println "Killing " (:name proc))
       (sh/destroy proc))
-    (leiningen.core.main/abort)))
+    (abort))
+
+(defn wait-for-early-exit [procs]
+  (deref
+    (apply primrose/first
+      (map #(future (sh/exit-code %)) procs))))
+
+(defn cooper [project & args]
+  (ensure-procfile-exists)
+  (doto (get-procs)
+        (pipe-procs)
+        (wait-for-early-exit)
+        (fail)))
