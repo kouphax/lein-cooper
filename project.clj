c56792d9c7c179f43c43517299626024fa4a71ef
Mike Schaeffer <mike@mschaef.com>
2016-01-29 22:29:57 -0500

    Update to a different version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-cooper "1.1.2"
+(defproject lein-cooper "1.2.1"
   :description "Run and combine multiple processes as single process"
   :url "https://github.com/kouphax/lein-cooper"
   :license {:name "Eclipse Public License"
